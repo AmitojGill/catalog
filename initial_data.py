01a34c067df1a7c24f88c09fe70b9fcc0b3f5751
Amitoj-Gill <amitoj.s.gill@gmail.com>
2018-01-23 15:37:43 -0800

    Add engine and connect to catalog.db

--- initial_data.py
+++ initial_data.py
@@ -1,3 +1,10 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from database_setup.py import Base, Categories, Items
+
+engine = create_engine('sqlite:///catalog.db')
+
+Base.metadata.bind = engine
+DBSession = sessionmaker(bind = engine)
+session = DBSession()
+
