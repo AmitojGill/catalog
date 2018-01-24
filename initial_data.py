01995b28875301a6e1d9382f3b60e4437983393a
Amitoj-Gill <amitoj.s.gill@gmail.com>
2018-01-23 22:20:22 -0800

    Add data

--- initial_data.py
+++ initial_data.py
@@ -12,11 +12,32 @@ session = DBSession()
 categories = ["Kitchen & Dining","Bed & Bath", "Appliances", 
 				"Garden & Outdoor", "Fine Art", "Pet Supplies"]
 
+#Delete any previus data
+delete_all = session.query(Categories).all()
+
+for i in delete_all:
+	session.delete(i)
+	session.commit()
+
+delete_items = session.query(Items).all()
+
+for i in delete_items:
+	session.delete(i)
+	session.commit()
+
 for i in categories:
 	category = Categories(name=i)
 	session.add(category)
 	session.commit()
 	print "%s added to the categories table" % i
 
+cat = session.query(Categories).all()
 
+for i in cat:
+	print i.name
 
+data = {"category":"Kitchen & Dining", "name":"Cookware", "description":"Gotham Steel 10-Piece Nonstick Frying Pan and Cookware Set - Graphite"}
+
+new_data = Items(name = data["name"], description = data["description"], category_id=data['category'])
+session.add(new_data)
+session.commit()
