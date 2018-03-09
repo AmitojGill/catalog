ca73a816e92d1b505f62b20e88dbb624d7203afa
Amitoj-Gill <amitoj.s.gill@gmail.com>
2018-03-08 21:12:02 -0800

    Stylesheet updated

--- application.py
+++ application.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route('/books')
 def books():
 	return render_template('main.html')
-
+	
 if __name__ == '__main__':
 	app.degug = True
 	app.run(host='0.0.0.0', port=8000)
